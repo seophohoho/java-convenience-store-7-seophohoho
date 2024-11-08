eebe4901325f6fdf3081abcb83fcf99a8250dbe7
seophohoho <seop0937@gmail.com>
2024-11-08 16:26:21 +0900

    feat(Util): 구분자를 기준으로 문자열 분리 메소드 추가

--- src/main/java/store/Utils.java
+++ src/main/java/store/Utils.java
@@ -8,10 +8,15 @@ import java.io.FileReader;
 public class Utils {
     private static final String basePath = "./src/main/resources/";
 
+    //나중에 try-catch로 FileNotFoundException 처리도 좀 해야 할듯.
     public static BufferedReader readFile(String filename) throws FileNotFoundException {
         File file = new File (basePath+filename);
         FileReader fileReader = new FileReader(file);
 
         return new BufferedReader(fileReader);
+    }
+
+    public static String[] separateStr(String str,String separator) {
+        return str.split(separator);
     }
 }
