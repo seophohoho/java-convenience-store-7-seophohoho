3218bbcec1cd2598eabb373f8406248ec80eb7b5
seophohoho <seop0937@gmail.com>
2024-11-08 16:33:21 +0900

    feat(Store): products.md 파일 읽기 및 데이터 저장 메소드 추가

--- src/main/java/store/model/Product.java
+++ src/main/java/store/model/Product.java
@@ -1,2 +1,47 @@
-package store.model;public class Product {
+package store.model;
+
+public class Product {
+    private String name;
+    private int price;
+    private int quantity;
+    private String promotion;
+
+    public Product(String name, int price, int quantity, String promotion){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.promotion = promotion;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getPromotion() {
+        return promotion;
+    }
+
+    public void setPromotion(String promotion) {
+        this.promotion = promotion;
+    }
 }
--- src/main/java/store/model/Store.java
+++ src/main/java/store/model/Store.java
@@ -1,10 +1,31 @@
 package store.model;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import store.Utils;
 
-public class Convenience {
+public class Store {
     List<Product> products = new ArrayList<>();
+    private static final String SEPARATOR = ",";
+    private static final String FILENAME_PRODUCT = "products.md";
+    private static final String FILENAME_PROMOTION = "promotions.md";
 
-    
+    public List<Product> getProducts(){
+        return products;
+    }
+
+    public void initProduct() throws IOException {
+        BufferedReader br = Utils.readFile(FILENAME_PRODUCT);
+        br.readLine();
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] separate = Utils.separateStr(line,SEPARATOR);
+            products.add(new Product(separate[0],Integer.parseInt(separate[1]),Integer.parseInt(separate[2]),separate[3]));
+        }
+
+        br.close();
+    }
 }
