838b481cea2cffa891264aee64d4e426270a5965
seophohoho <seop0937@gmail.com>
2024-11-08 16:35:16 +0900

    test(Store): promotion.md 파일을 읽고, promotion 저장 테스트 코드 추가

--- src/test/java/store/StoreTest.java
+++ src/test/java/store/StoreTest.java
@@ -6,8 +6,10 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import store.model.Product;
+import store.model.Promotion;
 import store.model.Store;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StoreTest {
     @Test
@@ -37,5 +39,27 @@ public class StoreTest {
                 .usingRecursiveFieldByFieldElementComparator()
                 .containsExactlyElementsOf(expected);
 
+    }
+
+    @Test
+    @DisplayName("promotion.md 파일을 읽고, promotion 저장 테스트")
+    void testPromotion() throws IOException {
+        Store store = new Store();
+        store.initPromotion();
+        List<List<Object>> expected = List.of(
+                List.of("탄산2+1",2,1,"2024-01-01","2024-12-31"),
+                List.of("MD추천상품",1,1,"2024-01-01","2024-12-31"),
+                List.of("반짝할인",1,1,"2024-11-01","2024-11-30")
+        );
+
+        int idx=0;
+        for(Promotion promotion: Promotion.values()){
+            assertEquals(expected.get(idx).get(0),promotion.getName());
+            assertEquals(expected.get(idx).get(1),promotion.getBuy());
+            assertEquals(expected.get(idx).get(2),promotion.getGet());
+            assertEquals(expected.get(idx).get(3),promotion.getStart());
+            assertEquals(expected.get(idx).get(4),promotion.getEnd());
+            idx++;
+        }
     }
 }
