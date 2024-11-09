8d51e6a214495d2e467f2c881ec42db669f08b6a
seophohoho <seop0937@gmail.com>
2024-11-09 18:51:26 +0900

    feat(Validator):  입력 컨벤션 유효성 판단 메소드 추가

--- src/main/java/store/model/Validator.java
+++ src/main/java/store/model/Validator.java
@@ -1,2 +1,19 @@
-package store.model;public class Validator {
+package store.model;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import store.view.Error;
+
+public class Validator {
+    private static final String ORDER_PATTERN = "\\[([가-힣]*|[[a-zA-Z]]*+)-([1-9][0-9]*+)](,\\[([가-힣]*|[[a-zA-Z]]*+)-([1-9][0-9]*+)])*";
+
+    public boolean orderValidate(String input){
+        Pattern patternOrder = Pattern.compile(ORDER_PATTERN);
+        Matcher matcherOrder = patternOrder.matcher(input);
+
+        if(!matcherOrder.matches()){
+            Error.reject(Error.INVALID_INPUT_MSG);
+        }
+        return true;
+    }
 }
