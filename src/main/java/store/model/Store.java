b1866a43b5b36f1bf4bd04ac5f536c68df7ad445
seophohoho <seop0937@gmail.com>
2024-11-09 23:29:20 +0900

    style(Store): 불필요한 import 삭제

--- src/main/java/store/model/Store.java
+++ src/main/java/store/model/Store.java
@@ -2,9 +2,7 @@ package store.model;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import store.Utils;
 
