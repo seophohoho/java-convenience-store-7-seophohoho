d54d0b4c4d2da194e5b8432c97874f7ddd497ff7
seophohoho <seop0937@gmail.com>
2024-11-09 17:51:20 +0900

    test(Validator): 물품 주문 입력 검증 테스트 코드 추가

---
+++ src/test/java/store/ValidatorTest.java
@@ -0,0 +1,69 @@
+package store;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import store.model.Validator;
+
+
+public class ValidatorTest {
+    @Test
+    @DisplayName("물품 주문 입력 검증 테스트_1개의 주문에 대해서")
+    void testOrderValidate_1(){
+        Validator validator = new Validator();
+
+        assertThatThrownBy(() -> validator.orderValidate(""))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> validator.orderValidate(" "))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> validator.orderValidate("콜라-"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> validator.orderValidate("콜라"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> validator.orderValidate("콜라-10"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> validator.orderValidate("[콜라-10],,"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> validator.orderValidate("[콜라-10],"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> validator.orderValidate(" [콜라-10]"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> validator.orderValidate(" [콜라-10] "))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> validator.orderValidate("[콜라-10"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> validator.orderValidate("[콜라-dwdwdw10]"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> validator.orderValidate("[콜라-dwdwdw10] "))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> validator.orderValidate("[콜라-dwdwdw10 ]"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("물품 주문 입력 검증 테스트_2개 이상의 주문에 대해서")
+    void testOrderValidate_2(){
+        Validator validator = new Validator();
+
+        assertThatThrownBy(() -> validator.orderValidate("[콜라-10],[사이다-10"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> validator.orderValidate("[콜라-10],사이다-10"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> validator.orderValidate("[콜라-10],[사이다-10],[비타민워터]"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
