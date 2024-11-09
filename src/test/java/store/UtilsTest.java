46cef0ea305f4d420ad0ebcb1b23b1487a145f08
seophohoho <seop0937@gmail.com>
2024-11-10 01:11:50 +0900

    test(Utils): 문자열을 LocalDateTime 변환 테스트 코드 추가

--- src/test/java/store/UtilsTest.java
+++ src/test/java/store/UtilsTest.java
@@ -2,6 +2,7 @@ package store;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import java.time.LocalDateTime;
 import java.time.Month;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,13 @@ public class UtilsTest {
     @DisplayName("현재 시간 반환 테스트")
     void testGetNow(){
         assertThat(Utils.getDateTimeNow()).hasYear(2024).hasMonth(Month.NOVEMBER).hasDayOfMonth(10);
+    }
+
+    @Test
+    @DisplayName("문자열을 LocalDateTime 변환 테스트")
+    void testStrToLocalDateTime() {
+        assertThat(Utils.strToLocalDateTime("2024-11-10")).isEqualTo(LocalDateTime.of(2024, 11, 10, 0, 0));
+        assertThat(Utils.strToLocalDateTime("2024-12-25")).isEqualTo(LocalDateTime.of(2024, 12, 25, 0, 0));
     }
 
 }
