368d6ce0d52def69f817ed96ed47595880d922c3
seophohoho <seop0937@gmail.com>
2024-11-11 16:05:57 +0900

    feat(Input): 프로모션_제품_수량 부족할 경우, 안내문 출력

--- src/main/java/store/view/Input.java
+++ src/main/java/store/view/Input.java
@@ -14,4 +14,9 @@ public class Input {
         System.out.printf("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)%n", product, remainder);
         return Console.readLine();
     }
+
+    public static String promotionNotApplyChoice(String product, int value){
+        System.out.printf("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)%n", product, value);
+        return Console.readLine();
+    }
 }
