47809f8d321652601ac9bee267f383d92d06c9d1
seophohoho <seop0937@gmail.com>
2024-11-20 18:52:41 +0900

    feat(FileUtil): 파일 줄 읽는 메소드 추가

--- src/main/java/store/utils/FileUtil.java
+++ src/main/java/store/utils/FileUtil.java
@@ -1,2 +1,28 @@
-package store.utils;public class FileUtil {
+package store.utils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileUtil {
+    private static final String basePath = "./src/main/resources/";
+
+    public static List<String> readLine(String filename) throws IOException {
+        List<String> lines = new ArrayList<>();
+        File file = new File (basePath+filename);
+        FileReader fileReader = new FileReader(file);
+        BufferedReader br = new BufferedReader(fileReader);
+        String line;
+
+        br.readLine();
+        while ((line = br.readLine()) != null) {
+            lines.add(line);
+        }
+
+        return lines;
+    }
 }
