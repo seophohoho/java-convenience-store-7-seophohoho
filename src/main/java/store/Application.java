91d383b87301cb67820bcebaa21abacac769f336
seophohoho <seop0937@gmail.com>
2024-11-20 16:50:51 +0900

    ----------restart code----------

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,11 +1,8 @@
 package store;
 
 import java.io.IOException;
-import store.controller.Controller;
 
 public class Application {
     public static void main(String[] args) throws IOException {
-        Controller controller = new Controller();
-        controller.run();
     }
 }
