d227d0a35a96180798ce42f06c15b412a3662c35
seophohoho <seop0937@gmail.com>
2024-11-27 20:01:07 +0900

    feat(Validator): 주문 입력값 유효성 체크 기능 구현

--- src/main/java/store/models/ErrorGroup.java
+++ src/main/java/store/models/ErrorGroup.java
@@ -1,15 +1,26 @@
 package store.models;
 
-public enum Error {
-    WRONG_INPUT(0,"올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."),
-    INVALID_FORM(1,),
-    NOTHING_PRODUCT(),
-    OUT_OF_STOCK(),
+import java.util.Arrays;
+
+public enum ErrorGroup {
+    WRONG_INPUT(0, "잘못된 입력입니다. 다시 입력해 주세요."),
+    INVALID_FORM(1, "올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."),
+    NOTHING_PRODUCT(2, "존재하지 않는 상품입니다. 다시 입력해 주세요."),
+    OUT_OF_STOCK(3, "재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요."),
+    EMPTY(-1, "NULL");
 
     private int code;
     private String message;
 
-    Error()
+    ErrorGroup(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
 
-    public
+    public static ErrorGroup findByErrorGroup(int code) {
+        return Arrays.stream(ErrorGroup.values())
+                .filter(group -> group.code == code)
+                .findAny()
+                .orElse(EMPTY);
+    }
 }
